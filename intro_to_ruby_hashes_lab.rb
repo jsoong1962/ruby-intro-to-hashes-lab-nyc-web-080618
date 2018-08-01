6bf3ca963435e1b15cd92906e1ad944be3bc8645
Johnny Soong <johnnysoong@hotmail.com>
2018-08-01 21:09:54 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,6 @@ def monopoly_with_second_tier
 	    :pieces => 4,
 	    :names => {
 	      :reading_railroad => {
-	        "mortgage_value" => "$100"
 	      },
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
